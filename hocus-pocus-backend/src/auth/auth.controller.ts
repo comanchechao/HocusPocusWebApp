86be12801fbe3428a0592fac04108b455b02bfdf
Greez <imgreez@protonmail.com>
2023-06-03 12:53:45 +0330

    auth module added

---
+++ hocus-pocus-backend/src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  signup() {
+    return 'this is signup';
+  }
+}
