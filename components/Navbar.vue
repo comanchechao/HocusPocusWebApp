5b532b4863cb3b1d3bbc1b71bdb21e4a70d4e144
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-06-05 13:58:10 +0330

    Update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,39 +2,39 @@
   <div
     class="w-screen h-20 flex items-center justify-center px-10 bg-darkPurple"
   >
-    <div class="flex items-center justify-end">
+    <div class="flex items-center justify-start space-x-3 w-full">
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-dashed border-transparent hover:border-mainViolet rounded-sm hover:text-mainViolet text-mainRed"
-      >
-        <span> ورود </span>
-        <PhKeyhole :size="25" />
-      </button>
-    </div>
-    <div class="w-full flex justify-end items-center space-x-3">
-      <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-dashed border-transparent hover:border-mainViolet rounded-sm hover:text-mainViolet text-mainRed"
-      >
-        <span> درباره ما </span>
-        <PhSpade />
-      </button>
-      <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-dashed border-transparent hover:border-mainViolet rounded-sm hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       >
         <span> تماس با ما </span> <PhCallBell />
       </button>
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-dashed border-transparent hover:border-mainViolet rounded-sm hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+      >
+        <span> درباره ما </span>
+        <PhSpade />
+      </button>
+    </div>
+    <div class="w-full flex justify-end items-center space-x-3">
+      <button
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainOrange rounded-sm shadow-md shadow-transparent hover:shadow-mainOrange hover:text-mainOrange text-mainYellow"
       >
         <span> آموزش </span>
         <PhCube />
       </button>
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-dashed border-transparent hover:border-mainViolet rounded-sm hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+      >
+        <span> ورود </span>
+        <PhKeyhole :size="25" />
+      </button>
+      <button
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       >
         <span> خرید </span> <PhStorefront />
       </button>
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-dashed border-transparent hover:border-mainViolet rounded-sm hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       >
         <span> خونه </span>
         <PhHouseSimple />
