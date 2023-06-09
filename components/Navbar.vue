fda969dd7d2547100d752ffa62774f37ad8545b7
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-06-09 19:15:14 +0330

    Progress

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,12 +4,12 @@
   >
     <div class="flex items-center justify-start space-x-3 w-full">
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       >
         <span> تماس با ما </span> <PhCallBell />
       </button>
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       >
         <span> درباره ما </span>
         <PhSpade />
@@ -17,24 +17,24 @@
     </div>
     <div class="w-full flex justify-end items-center space-x-3">
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainOrange rounded-sm shadow-md shadow-transparent hover:shadow-mainOrange hover:text-mainOrange text-mainYellow"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainOrange rounded-sm shadow-md shadow-transparent hover:shadow-mainOrange hover:text-mainOrange text-mainYellow"
       >
         <span> آموزش </span>
         <PhCube />
       </button>
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       >
         <span> ورود </span>
         <PhKeyhole :size="25" />
       </button>
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       >
         <span> خرید </span> <PhStorefront />
       </button>
       <button
-        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition bg-main duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+        class="text-xl flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       >
         <span> خونه </span>
         <PhHouseSimple />
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,61 @@
 <template>
-  <div>
+  <div class="w-screen">
     <Navbar></Navbar>
     <div
-      class="h-screen text-9xl w-screen flex items-center justify-center bg-darkPurple"
+      class="h-screen text-9xl w-screen flex items-start pt-7 justify-center bg-darkPurple px-16"
     >
-      <!-- <h1 class="text-mainRed">Hocus Pocus</h1> -->
+      <div class="w-1/2 h-dialog flex items-center justify-center py-3 px-14">
+        <div
+          class="transition h-full w-full duration-150 ease-in-out border-2 cursor-pointer border-mainViolet rounded-md shadow-xl shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+        ></div>
+      </div>
+      <div
+        class="w-1/2 h-dialog flex flex-col space-y-24 items-center justify-center"
+      >
+        <h1
+          class="transition flex duration-150 ease-in-out text-2xl border-mainViolet drop-shadow-sm rounded-sm text-mainRed"
+        >
+          <PhMagicWand />
+          <span>خوش اومدید به مجیک شاپِ </span>
+        </h1>
+        <h1
+          class="transition HocusPocus duration-150 ease-in-out text-9xl rounded-sm text-white"
+        >
+          Hocus Pocus
+        </h1>
+      </div>
+    </div>
+    <div class="h-screen w-full bg-darkPurple flex">
+      <div class="h-24 w-screen bg-mainRed flex items-center justify-center">
+        <h2 class="text-darkPurple text-3xl flex">
+          <PhCards class="mr-3" /> <span> جدیدترین کارت ها </span>
+        </h2>
+      </div>
     </div>
   </div>
 </template>
-<script></script>
-<style></style>
+<script setup>
+import { PhMagicWand, PhCards } from "@phosphor-icons/vue";
+</script>
+<style>
+@font-face {
+  font-family: "Hocus";
+  src: url("../assets/fonts/Satisfy-Regular.ttf");
+  font-style: normal;
+  font-display: swap;
+
+  /* unicode-range: U+0020-007F; */
+}
+
+.HocusPocus {
+  font-family: "Hocus";
+  text-shadow: 0 0 1px #f72585, 0 0 4px #f72585, 0 0 10px #f72585,
+    0 0 20px #f72585;
+}
+
+.neonText {
+  color: #fff;
+  text-shadow: 0 0 1px #f72585, 0 0 4px #f72585, 0 0 10px #f72585,
+    0 0 20px #f72585;
+}
+</style>
