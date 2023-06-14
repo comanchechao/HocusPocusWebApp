8f6faf0a9c53c2284fd4ce54022e1c6ced3a6783
Greez <imgreez@protonmail.com>
2023-06-14 17:52:07 +0330

    type ORM module and conf

--- hocus-pocus-backend/src/auth/auth.service.ts
+++ hocus-pocus-backend/src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {
-    return {msg: 'i am signup mate'};
+    return { msg: 'i am signup mate' };
   }
 }
