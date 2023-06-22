189ccd54e576a3aa878f33fbce219faba6ca9cee
Greez <imgreez@protonmail.com>
2023-06-22 17:40:19 +0330

    steps

--- hocus-pocus-backend/src/auth/auth.module.ts
+++ hocus-pocus-backend/src/auth/auth.module.ts
@@ -5,10 +5,11 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session.serielizer';
 
 @Module({
-  imports: [PrismaModule, PassportModule],
+  imports: [PrismaModule, PassportModule.register({ session: true })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
