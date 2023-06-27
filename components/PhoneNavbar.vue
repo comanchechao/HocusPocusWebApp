1d205098be35189105cbd85ac0288e00c15e9a6c
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-06-27 09:48:53 +0330

    Update PhoneNavbar.vue

--- components/PhoneNavbar.vue
+++ components/PhoneNavbar.vue
@@ -1,6 +1,43 @@
 <template>
   <div class="">
-    <Sidebar position="left" v-model:visible="visible"> </Sidebar>
+    <Sidebar position="left" v-model:visible="visible">
+      <div
+        class="h-full w-full bg-darkPurple flex flex-col items-center justify-start space-y-5"
+      >
+        <NuxtLink to="/">
+          <button
+            class="text-xl flex active:text-mainRed active:bg-darkPurple items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent bg-mainRed hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-darkPurple text-darkPurple"
+          >
+            <span> خونه </span>
+            <PhHouseSimple :size="23" />
+          </button>
+        </NuxtLink>
+        <NuxtLink to="/shop/shop">
+          <button
+            class="text-xl active:text-darkPurple active:bg-mainRed flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+          >
+            <span> خرید </span> <PhStorefront :size="23" />
+          </button>
+        </NuxtLink>
+        <button
+          class="text-xl active:text-darkPurple active:bg-mainOrange flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainOrange rounded-sm shadow-md shadow-transparent hover:shadow-mainOrange hover:text-mainOrange text-mainYellow"
+        >
+          <span> آموزش </span>
+          <PhCube :size="23" />
+        </button>
+        <button
+          class="text-xl active:text-darkPurple active:bg-mainRed flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+        >
+          <span> تماس با ما </span> <PhCallBell :size="23" />
+        </button>
+        <button
+          class="text-xl active:text-darkPurple active:bg-mainRed flex items-center space-x-2 px-8 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
+        >
+          <span> درباره ما </span>
+          <PhSpade :size="23" />
+        </button>
+      </div>
+    </Sidebar>
     <button
       class="text-xl active:text-darkPurple active:bg-mainRed flex items-center space-x-2 px-3 py-1 transform scale-100 hover:scale-105 transition duration-150 ease-in-out border-2 border-transparent hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-mainViolet text-mainRed"
       @click="visible = true"
@@ -11,8 +48,22 @@
 </template>
 
 <script setup>
-import { PhList } from "@phosphor-icons/vue";
 import { ref } from "vue";
-
+import {
+  PhHouseSimple,
+  PhCube,
+  PhCallBell,
+  PhSpade,
+  PhStorefront,
+  PhList,
+} from "@phosphor-icons/vue";
 const visible = ref(false);
 </script>
+<style>
+.p-sidebar .p-sidebar-content {
+  padding: 0;
+}
+.p-sidebar .p-sidebar-header {
+  background-color: #10002b;
+}
+</style>
