a9f76cccb167e2e0b2321da1e1f499dbbfe82ac1
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-09-18 01:49:36 +0330

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,7 +6,7 @@
       'bg-darkPurple':
         $route.path === '/shop/shop' || $route.path.startsWith('/shop/'),
     }"
-    class="lg:h-rem77 h-full overflow-hidden w-screen flex-col flex items-center space-y-8 justify-center"
+    class="lg:h-full h-full overflow-hidden w-screen flex-col flex items-center space-y-8 justify-center"
   >
     <!-- <img
       src="../assets/images/MagicDivider2.webp"
@@ -22,11 +22,52 @@
           class="w-full h-44 object-contain"
           alt=""
         />
-        <h2 class="text-lg text-mainPurple neonText">
-          فروشگاه لوازم شعبده بازی
+        <h2 class="text-lg text-mainRed">
+          آنلاین شاپ و وبسایت آموزشی شعبده بازیِ
         </h2>
-        <h1 class="text-8xl neonText HocusText text-white">Hocus Pocus</h1>
-
+        <div class="w-full flex flex-col h-48 items-center justify-center">
+          <div class="flex flex-col items-center justify-center">
+            <div class="flex items-center absolute justify-center w-full">
+              <img
+                src="../assets/images/HocusUp1.webp"
+                class="object-contain absolute Up w-96"
+                alt=""
+              />
+              <img
+                src="../assets/images/HocusUp2.webp"
+                class="object-contain absolute Up2 w-96"
+                alt=""
+              />
+            </div>
+            <div class="flex items-center absolute justify-center w-full">
+              <img
+                src="../assets/images/HocusDownP.webp"
+                class="object-contain w-96 absolute P"
+                alt=""
+              />
+              <img
+                src="../assets/images/HocusDownO.webp"
+                class="object-contain w-96 absolute O"
+                alt=""
+              />
+              <img
+                src="../assets/images/HocusDownC.webp"
+                class="object-contain w-96 absolute C"
+                alt=""
+              />
+              <img
+                src="../assets/images/HocusDownU.webp"
+                class="object-contain w-96 absolute U"
+                alt=""
+              />
+              <img
+                src="../assets/images/HocusDownS.webp"
+                class="object-contain w-96 absolute S"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
         <div class="flex items-center justify-center space-x-5">
           <button
             class="text-mainRed cursor-pointer transition ease-in-out duration-150 p-2 shadow-lg shadow-transparent hover:shadow-mainPurple rounded-md"
