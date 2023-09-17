4c9e358b10d253d1d9631d302b3c102f561940f7
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-09-17 07:38:49 +0330

    index update

---
+++ components/IndexPopularMiniCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="h-full w-full bg-mainYellow space-y-4 flex flex-col items-end justify-end p-5"
+  >
+    <h2 class="text-darkPurple font-bold text-center text-md lg:text-xl flex">
+      <span> مجموعه آموزشی برای شروع </span>
+    </h2>
+    <h3
+      class="text-xl border-2 border-dashed border-darkPurple rounded-md py-1 px-5 text-mainBrown flex items-center justify-center space-x-2"
+    >
+      <span class="text-xs text-darkPurple">تومان</span>
+      <span>599,000</span>
+      <PhMoney :size="34" weight="fill" />
+    </h3>
+    <button
+      class="w-full justify-center mt-3 py-2 shadow-md bg-mainYellow shadow-mainYellow hover:shadow-mainOrange flex items-center space-x-2 transition text-sm duration-200 ease-in-out text-mainBrown hover:text-mainBrown border-2 border-mainBrown hover:bg-mainYellow rounded-md"
+    >
+      <span> اضافه به سبد خرید </span> <PhShoppingBagOpen :size="20" />
+    </button>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -210,7 +210,7 @@
         class="w-full h-screen grid grid-cols-4 grid-rows-2 place-items-center px-24 py-9 gap-5"
       >
         <div
-          class="h-full space-y-4 w-full bg-mainPink col-span-2 row-span-2 flex flex-col items-end justify-end p-5"
+          class="h-full space-y-4 w-full bg-white col-span-2 row-span-2 flex flex-col items-end justify-end p-5"
         >
           <h2
             class="text-darkPurple font-bold text-center text-lg lg:text-3xl flex"
@@ -224,72 +224,16 @@
             <span>599,000</span>
             <PhMoney :size="34" weight="fill" />
           </h3>
+          <button
+            class="w-full justify-center mt-3 py-3 shadow-md bg-mainYellow shadow-mainYellow hover:shadow-mainOrange flex items-center space-x-2 transition text-lg duration-200 ease-in-out text-mainBrown hover:text-mainBrown border-2 border-mainBrown hover:bg-mainYellow rounded-md"
+          >
+            <span> اضافه به سبد خرید </span> <PhShoppingBagOpen :size="20" />
+          </button>
         </div>
 
-        <div
-          class="h-full w-full bg-mainYellow space-y-4 flex flex-col items-end justify-end p-5"
-        >
-          <h2
-            class="text-darkPurple font-bold text-center text-md lg:text-xl flex"
-          >
-            <span> مجموعه آموزشی برای شروع </span>
-          </h2>
-          <h3
-            class="text-xl border-2 border-dashed border-darkPurple rounded-md py-1 px-5 text-mainBrown flex items-center justify-center space-x-2"
-          >
-            <span class="text-xs text-darkPurple">تومان</span>
-            <span>599,000</span>
-            <PhMoney :size="34" weight="fill" />
-          </h3>
-        </div>
-        <div
-          class="h-full w-full bg-mainYellow space-y-4 flex flex-col items-end justify-end p-5"
-        >
-          <h2
-            class="text-darkPurple font-bold text-center text-md lg:text-xl flex"
-          >
-            <span> مجموعه آموزشی برای شروع </span>
-          </h2>
-          <h3
-            class="text-xl border-2 border-dashed border-darkPurple rounded-md py-1 px-5 text-mainBrown flex items-center justify-center space-x-2"
-          >
-            <span class="text-xs text-darkPurple">تومان</span>
-            <span>599,000</span>
-            <PhMoney :size="34" weight="fill" />
-          </h3>
-        </div>
-        <div
-          class="h-full w-full bg-mainYellow space-y-4 flex flex-col items-end justify-end p-5"
-        >
-          <h2
-            class="text-darkPurple font-bold text-center text-md lg:text-xl flex"
-          >
-            <span> مجموعه آموزشی برای شروع </span>
-          </h2>
-          <h3
-            class="text-xl border-2 border-dashed border-darkPurple rounded-md py-1 px-5 text-mainBrown flex items-center justify-center space-x-2"
-          >
-            <span class="text-xs text-darkPurple">تومان</span>
-            <span>599,000</span>
-            <PhMoney :size="34" weight="fill" />
-          </h3>
-        </div>
-        <div
-          class="h-full w-full bg-mainYellow space-y-4 flex flex-col items-end justify-end p-5"
-        >
-          <h2
-            class="text-darkPurple font-bold text-center text-md lg:text-xl flex"
-          >
-            <span> مجموعه آموزشی برای شروع </span>
-          </h2>
-          <h3
-            class="text-xl border-2 border-dashed border-darkPurple rounded-md py-1 px-5 text-mainBrown flex items-center justify-center space-x-2"
-          >
-            <span class="text-xs text-darkPurple">تومان</span>
-            <span>599,000</span>
-            <PhMoney :size="34" weight="fill" />
-          </h3>
-        </div>
+        <LazyIndexPopularMiniCard /> <LazyIndexPopularMiniCard />
+        <LazyIndexPopularMiniCard />
+        <LazyIndexPopularMiniCard />
       </div>
     </div>
     <div class="w-full h-full my-14 flex flex-col">
