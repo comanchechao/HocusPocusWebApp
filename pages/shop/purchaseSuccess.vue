0dc2721484440368d948d537d6432a5124923162
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-09-25 04:02:56 +0330

    Create purchaseSuccess.vue

---
+++ pages/shop/purchaseSuccess.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="h-full w-screen bg-darkPurple">
+    <LazyNavbar />
+    <div
+      class="w-full h-screen lg:px-28 px-4 flex justify-start lg:items-center justify-center"
+    >
+      <div
+        class="h-rem30 w-rem28 bg-white rounded-md flex items-center flex-col justify-around"
+      >
+        <PhCheckCircle class="text-green-500" weight="fill" :size="230" />
+        <h1 class="lg:text-4xl text-3xl md:text-4xl text-green-500">
+          پرداخت موفقیت آمیز بود
+        </h1>
+        <h1
+          class="text-xl text-green-500 p-3 rounded-md border-2 border-dashed border-green-600"
+        >
+          کد پرداخت <span class="text-darkPurple">25445</span>
+        </h1>
+        <div class="flex items-center space-x-3">
+          <NuxtLink to="/shop/profile">
+            <button
+              class="text-lg flex active:text-mainRed active:bg-darkPurple items-center space-x-2 px-8 py-1 transition duration-150 ease-in-out border-2 border-transparent bg-mainRed hover:border-mainViolet rounded-sm shadow-md shadow-transparent hover:shadow-mainViolet hover:text-darkPurple text-darkPurple"
+            >
+              <span> پروفایل </span>
+              <PhAlien :size="23" />
+            </button>
+          </NuxtLink>
+          <h1 class="text-xl text-mainRed">پیگیری سفارش در</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { PhCheckCircle, PhAlien } from "@phosphor-icons/vue";
+</script>
