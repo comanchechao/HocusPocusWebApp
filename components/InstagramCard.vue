82ceeda1bf6e7e59c7b2694fde4f7eae52df4708
comanchechao <61477751+comanchechao@users.noreply.github.com>
2023-11-02 21:55:57 +0330

    Update InstagramCard.vue

--- components/InstagramCard.vue
+++ components/InstagramCard.vue
@@ -2,11 +2,11 @@
   <div
     class="w-full relative h-64 rounded-sm cursor-pointer bg-mainWhite flex items-center justify-center"
   >
-    <img
+    <!-- <img
       src="../assets/images/Psychonauts.webp"
       class="object-contain h-full p-3 Img"
       alt=""
-    />
+    /> -->
     <div
       class="absolute text-xl text-white w-full flex items-center Caption p-6 justify-center h-36 bg-black bg-opacity-80 bottom-0"
     >
