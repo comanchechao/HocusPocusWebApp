fa70dada4063a35c6ffc48b456654d33668ab24c
Greez <imgreez@protonmail.com>
2023-11-09 21:55:20 +0330

    update

--- hocus-pocus-backend/src/video-comments/video-comments.service.ts
+++ hocus-pocus-backend/src/video-comments/video-comments.service.ts
@@ -7,23 +7,23 @@ export class VideoCommentsService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async getProductComments(id: string) {
-    // const comments = await this.prismaService.videoComments.findMany({
-    //   where: {
-    //     course_id: Number(id),
-    //   },
-    // });
-    // return { comments: comments };
+    const comments = await this.prismaService.videoComments.findMany({
+      where: {
+        course_id: Number(id),
+      },
+    });
+    return { comments: comments };
   }
 
   async submitComment(id: string, dto: SubmitCommentDto) {
     console.log(id);
-    // const comment = await this.prismaService.videoComments.create({
-    //   data: {
-    //     course_id: Number(id),
-    //     user_id: Number(dto.userId),
-    //     comment: dto.comment,
-    //     username: dto.username,
-    //   },
-    // });
+    const comment = await this.prismaService.videoComments.create({
+      data: {
+        course_id: Number(id),
+        user_id: Number(dto.userId),
+        comment: dto.comment,
+        username: dto.username,
+      },
+    });
   }
 }
