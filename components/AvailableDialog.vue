90256489c713490970bccedcf1291152e6e9efce
Chao <61477751+comanchechao@users.noreply.github.com>
2024-04-08 21:34:39 +0330

    up

--- components/AvailableDialog.vue
+++ components/AvailableDialog.vue
@@ -43,9 +43,10 @@
             class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
           </label>
-          <div class="relative">
-            <input
-              v-model="search"
+          <div class="relative ">
+            <input 
+            dir="rtl"
+               v-model="search"
               type="search"
               id="default-search"
               class="block w-96 bg-mainWhite p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -61,21 +62,7 @@
             <div
               class="absolute inset-y-0 end-0 flex pr-4 items-center ps-3 pointer-events-none"
             >
-              <svg
-                class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
+             
             </div>
           </div>
         </div>
--- components/InStockDialog.vue
+++ components/InStockDialog.vue
@@ -44,7 +44,8 @@
           >
           </label>
           <div class="relative">
-            <input
+            <input             dir="rtl"
+
               v-model="search"
               type="search"
               id="default-search"
@@ -61,21 +62,7 @@
             <div
               class="absolute inset-y-0 end-0 flex pr-4 items-center ps-3 pointer-events-none"
             >
-              <svg
-                class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
+             
             </div>
           </div>
         </div>
